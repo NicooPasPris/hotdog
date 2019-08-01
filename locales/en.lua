a5d323b950dc7dc2ea3548e8ffd95d415c955038
NicooPasPris <ncobrailmc@gmail.com>
2019-08-01 14:58:11 +0200

    Add files via upload
    Russian and English translation

---
+++ locales/en.lua
@@ -0,0 +1,8 @@
+Locales['en'] = {
+  ['hotdog_shop_prompt'] 	= 'Press ~INPUT_PICKUP~ to order a ~y~Hot Dog~s~.',
+  ['bought']             	= 'And here is your ~b~Hot Dog~s~. That will make you ~o~',
+  ['not_enough']   			= 'You don\'t have ~r~enough~w~ money.',
+  ['max_item']           	= 'You are already enough about yourself.',
+  ['command_in_progress'] 	= 'Your ~y~order~s~ is ~r~being prepared~s~.',
+  ['used_hotdog']			= 'You eat a ~b~ Hot Dog',
+}
