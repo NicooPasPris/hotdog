554a413cfe45717b42668facb1821a9fc592b559
NicooPasPris <ncobrailmc@gmail.com>
2019-08-02 19:32:39 +0200

    Add files via upload
    Turkish translation

---
+++ locales/tr.lua
@@ -0,0 +1,8 @@
+Locales['tr'] = {
+  ['hotdog_shop_prompt'] 	= '~y~Hot Dog~s~ satın almak için ~INPUT_PICKUP~ tuşuna basın.', 
+  ['bought']             	= '~b~Hot Dog~s~ satın aldın.',
+  ['not_enough']   			= 'Satın almak için yeterli paran yok.',
+  ['max_item']           	= 'Envanterine daha fazla Hot Dog alamazsın.',
+  ['command_in_progress'] 	= 'Siparişiniz hazırlık aşamasındadır.',
+  ['used_hotdog']			= 'Hot Dog'un tadıda efsaneymiş!'
+}
