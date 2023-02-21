1fdd56dbf92eb2c09ffb8d8fd258e6dbe38698c0
reverseHaze <96739561+reverseHaze@users.noreply.github.com>
2023-02-21 03:09:44 +0100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -17,10 +17,7 @@ local CurrentStand = nil
 
 
 Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
+	ESX = exports['es_extended']:getSharedObject()
 end)
 
 
