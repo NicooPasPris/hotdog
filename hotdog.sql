b6830ac82bf9c3bc6f5db27a9aaffa8adb6abce7
reverseHaze <96739561+reverseHaze@users.noreply.github.com>
2023-02-21 03:06:12 +0100

    Update hotdog.sql

--- hotdog.sql
+++ hotdog.sql
@@ -1,1 +1,1 @@
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('hotdog', 'HotDog', '5', '0', '1');
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES ('hotdog', 'HotDog', 5, false, true);
